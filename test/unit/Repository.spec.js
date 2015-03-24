a065c31cc4ed97d9f84f195ca8d2767ba12a201a
Darlan Alves <me@darlanalv.es>
2015-03-23 22:31:00 -0300

    test(Repository): refactor tests and cleanup old mockups

--- test/unit/Repository.spec.js
+++ test/unit/Repository.spec.js
@@ -2,218 +2,248 @@ describe('Repository', function() {
 
 	beforeEach(module('repository'));
 
-	// tests with a mocked DataProviderInterface
-	describe(' ', function() {
-		var instance;
+	var instance;
 
-		function DataProviderInterface() {}
+	beforeEach(inject(function(DataProviderInterface, RepositoryConfig, Repository) {
+		var DataProvider = DataProviderInterface.extend();
 
-		function RepositoryConfig() {
-			this.dataProvider = new DataProviderInterface();
-		}
-
-		beforeEach(module({
-			RepositoryConfig: RepositoryConfig,
-			DataProviderInterface: DataProviderInterface
-		}));
-
-		beforeEach(inject(function(RepositoryConfig, Repository) {
-			var config = new RepositoryConfig();
-			instance = new Repository(config);
-		}));
-
-		describe('::extend(Object [prototype])', function() {
-			it('should be a static method to extend the repository class', inject(function(Repository, RepositoryConfig) {
-				expect(typeof Repository.extend).toBe('function');
-
-				var config = new RepositoryConfig();
-				var Dummy = Repository.extend({
-					isDummy: true
-				});
-
-				var dummy = new Dummy(config);
-				expect(dummy instanceof Repository).toBe(true);
-				expect(dummy.isDummy).toBe(true);
-			}));
+		var config = new RepositoryConfig({
+			name: 'resource',
+			dataProvider: new DataProvider()
 		});
 
-		describe('#constructor(Object config)', function() {
-			it('should NOT continue without a valid config', inject(function(Repository) {
-				function invalidConfig() {
-					return new Repository(null);
-				}
+		instance = new Repository(config);
+	}));
 
-				expect(invalidConfig).toThrow();
-			}));
+	describe('::extend(Object [prototype])', function() {
+		it('should be a static method to extend the repository class', inject(function(Repository, RepositoryConfig, DataProviderInterface) {
+			expect(typeof Repository.extend).toBe('function');
 
-			it('should save the config into .config property', inject(function(RepositoryConfig) {
-				expect(instance.config).not.toBe(undefined);
-				expect(instance.config instanceof RepositoryConfig).toBe(true);
-			}));
-		});
-
-		describe('#createContext(String name)', function() {
-			it('should create a Repository Context with a given name', inject(function(RepositoryContext) {
-				var context = instance.createContext('context');
-				expect(context).not.toBeFalsy();
-				expect(context instanceof RepositoryContext).toBe(true);
-			}));
-
-			it('should NOT create a new context if one with the same name already exists. Return it instead', inject(function(RepositoryContext) {
-				var context = instance.createContext('my-context');
-				expect(context instanceof RepositoryContext).toBe(true);
-
-				var otherContext = instance.createContext('my-context');
-				expect(otherContext).toBe(context);
-			}));
-		});
-
-		describe('#getContext(String name)', function() {
-			it('should return null if the context was not found', function() {
-				var context = instance.getContext('null-context');
-				expect(context).toBe(null);
+			var config = new RepositoryConfig({
+				name: 'static-test',
+				dataProvider: new DataProviderInterface()
 			});
 
-			it('should return a context that was created before with the given name', inject(function(RepositoryContext) {
-				var context = instance.createContext('context');
-				var foundContext = instance.getContext('context');
-				expect(foundContext).not.toBe(null);
-				expect(foundContext).toBe(context);
-				expect(foundContext instanceof RepositoryContext).toBe(true);
-			}));
+			var Dummy = Repository.extend({
+				isDummy: true
+			});
+
+			var dummy = new Dummy(config);
+			expect(dummy instanceof Repository).toBe(true);
+			expect(dummy.isDummy).toBe(true);
+		}));
+	});
+
+	describe('#constructor(Object config)', function() {
+		it('should NOT continue without a valid config', inject(function(Repository) {
+			function invalidConfig() {
+				return new Repository(null);
+			}
+
+			expect(invalidConfig).toThrow();
+		}));
+
+		it('should save the config into .config property', inject(function(RepositoryConfig) {
+			expect(instance.config).not.toBe(undefined);
+			expect(instance.config instanceof RepositoryConfig).toBe(true);
+		}));
+	});
+
+	describe('#createContext(String name)', function() {
+		it('should create a Repository Context with a given name', inject(function(RepositoryContext) {
+			var context = instance.createContext('context');
+			expect(context).not.toBeFalsy();
+			expect(context instanceof RepositoryContext).toBe(true);
+		}));
+
+		it('should NOT create a new context if one with the same name already exists. Return it instead', inject(function(RepositoryContext) {
+			var context = instance.createContext('my-context');
+			expect(context instanceof RepositoryContext).toBe(true);
+
+			var otherContext = instance.createContext('my-context');
+			expect(otherContext).toBe(context);
+		}));
+	});
+
+	describe('#getContext(String name)', function() {
+		it('should return null if the context was not found', function() {
+			var context = instance.getContext('null-context');
+			expect(context).toBe(null);
 		});
 
-		describe('#removeContext(String name)', function() {
-			it('should destroy a previously created context', inject(function(Repository, RepositoryContext) {
-				var context = instance.createContext('context');
-				expect(context instanceof RepositoryContext).toBe(true);
+		it('should return a context that was created before with the given name', inject(function(RepositoryContext) {
+			var context = instance.createContext('context');
+			var foundContext = instance.getContext('context');
+			expect(foundContext).not.toBe(null);
+			expect(foundContext).toBe(context);
+			expect(foundContext instanceof RepositoryContext).toBe(true);
+		}));
+	});
 
-				instance.removeContext('context');
-				context = instance.getContext('context');
-				expect(context).toBe(null);
-			}));
+	describe('#removeContext(String name)', function() {
+		it('should destroy a previously created context', inject(function(Repository, RepositoryContext) {
+			var context = instance.createContext('context');
+			expect(context instanceof RepositoryContext).toBe(true);
+
+			instance.removeContext('context');
+			context = instance.getContext('context');
+			expect(context).toBe(null);
+		}));
+	});
+	// });
+
+	// --- ///
+	describe('#findOne(String id)', function() {
+		it('should retrieve a single entity with the given id through DataProvider#findOne', inject(function(RepositoryConfig, $q, $rootScope) {
+			var config = instance.config,
+				dataProvider = config.dataProvider,
+				id = 'entity-id',
+
+				entity = {
+					id: id,
+					name: 'John'
+				};
+
+			spyOn(dataProvider, 'findOne').and.returnValue($q.when(entity));
+
+			var promise = instance.findOne(id),
+				value;
+
+			expect(dataProvider.findOne).toHaveBeenCalledWith(instance.config.name, id);
+
+			promise.then(function(e) {
+				value = e;
+			});
+
+			$rootScope.$digest();
+			expect(value).toEqual(entity);
+		}));
+
+		it('should NOT search if the method is not allowed on dataProvider', function() {
+			var dataProvider = instance.config.dataProvider;
+			spyOn(dataProvider, 'canGet').and.returnValue(false);
+			spyOn(dataProvider, 'findOne');
+
+			instance.findOne('id');
+
+			expect(dataProvider.canGet).toHaveBeenCalled();
+			expect(dataProvider.findOne).not.toHaveBeenCalled();
 		});
 	});
 
-	describe('  ', function() {
-		var DataProvider;
-		var instance;
+	describe('#save(Object entity)', function() {
+		it('should persist the entity values', inject(function($q, $rootScope) {
+			var config = instance.config,
+				dataProvider = config.dataProvider,
+				entity = {
+					id: 'entity-id',
+					name: 'John'
+				};
 
-		beforeEach(inject(function(DataProviderInterface, RepositoryConfig, Repository) {
-			DataProvider = DataProviderInterface.extend();
+			spyOn(dataProvider, 'save').and.returnValue($q.when(true));
+			instance.save(entity);
+			$rootScope.$digest();
 
-			var config = new RepositoryConfig({
-				name: 'resource',
-				dataProvider: new DataProvider()
-			});
-
-			instance = new Repository(config);
+			expect(dataProvider.save).toHaveBeenCalledWith(instance.config.name, entity);
 		}));
 
-		describe('#findOne(String id)', function() {
-			it('should retrieve a single entity with the given id through DataProvider#findOne', inject(function(RepositoryConfig, $q, $rootScope) {
-				var config = instance.config,
-					dataProvider = config.dataProvider,
-					id = 'entity-id',
+		it('should NOT save if the method is not allowed on dataProvider', function() {
+			var dataProvider = instance.config.dataProvider;
+			spyOn(dataProvider, 'canSave').and.returnValue(false);
+			spyOn(dataProvider, 'save');
 
-					entity = {
-						id: id,
-						name: 'John'
-					};
-
-				spyOn(dataProvider, 'findOne').and.returnValue($q.when(entity));
-
-				var promise = instance.findOne(id),
-					value;
-
-				expect(dataProvider.findOne).toHaveBeenCalledWith(instance.config.name, id);
-
-				promise.then(function(e) {
-					value = e;
-				});
-
-				$rootScope.$digest();
-				expect(value).toEqual(entity);
-			}));
-
-			it('should NOT search if the method is not allowed on dataProvider', function() {
-				var dataProvider = instance.config.dataProvider;
-				spyOn(dataProvider, 'canGet').and.returnValue(false);
-				spyOn(dataProvider, 'findOne');
-
-				instance.findOne('id');
-
-				expect(dataProvider.canGet).toHaveBeenCalled();
-				expect(dataProvider.findOne).not.toHaveBeenCalled();
+			instance.save({
+				id: '1'
 			});
+
+			expect(dataProvider.canSave).toHaveBeenCalled();
+			expect(dataProvider.save).not.toHaveBeenCalled();
 		});
+	});
 
-		describe('#save(Object entity)', function() {
-			it('should persist the entity values', inject(function($q, $rootScope) {
-				var config = instance.config,
-					dataProvider = config.dataProvider,
-					entity = {
-						id: 'entity-id',
-						name: 'John'
-					};
+	describe('#remove(String id)', function() {
+		it('should remove a single entity found by ID', inject(function($q, $rootScope) {
+			var config = instance.config,
+				dataProvider = config.dataProvider,
+				entityId = 'entity-id';
 
-				spyOn(dataProvider, 'save').and.returnValue($q.when(true));
-				instance.save(entity);
-				$rootScope.$digest();
+			spyOn(dataProvider, 'remove').and.returnValue($q.when(true));
+			instance.remove(entityId);
+			$rootScope.$digest();
 
-				expect(dataProvider.save).toHaveBeenCalledWith(instance.config.name, entity);
-			}));
+			expect(dataProvider.remove).toHaveBeenCalledWith(instance.config.name, entityId);
+		}));
 
-			it('should NOT save if the method is not allowed on dataProvider', function() {
-				var dataProvider = instance.config.dataProvider;
-				spyOn(dataProvider, 'canSave').and.returnValue(false);
-				spyOn(dataProvider, 'save');
+		it('should NOT remove if the method is not allowed on dataProvider', function() {
+			var dataProvider = instance.config.dataProvider;
+			spyOn(dataProvider, 'canRemove').and.returnValue(false);
+			spyOn(dataProvider, 'remove');
 
-				instance.save({
-					id: '1'
-				});
+			instance.remove('id');
 
-				expect(dataProvider.canSave).toHaveBeenCalled();
-				expect(dataProvider.save).not.toHaveBeenCalled();
+			expect(dataProvider.canRemove).toHaveBeenCalled();
+			expect(dataProvider.remove).not.toHaveBeenCalled();
+		});
+	});
+
+	describe('#createQuery()', function() {
+		it('should create a instance of QueryBuilder bound to the repository', inject(function(QueryBuilder) {
+			var query = instance.createQuery();
+			expect(query instanceof QueryBuilder).toBe(true);
+			expect(query.$$repository).toBe(instance.config.name);
+		}));
+	});
+
+	describe('#findAll(QueryBuilder query)', function() {
+		it('should call the dataProvider with the query parameters and return a promise', inject(function($q, QueryBuilder) {
+			var dataProvider = instance.dataProvider;
+			var response = {
+				meta: {
+					count: 100,
+					itemsPerPage: 2,
+					currentPage: 1
+				},
+				data: [{}, {}]
+			};
+
+			var qb = QueryBuilder.create()
+				.from(instance.name)
+				.limit(2)
+				.skip(0);
+
+			spyOn(dataProvider, 'findAll').and.returnValue($q.when(response));
+
+			instance.findAll(qb);
+			var args = dataProvider.findAll.calls.argsFor(0);
+
+			expect(args[0]).toBe(qb.$$repository);
+
+			expect(args[1].pagination).toEqual({
+				currentPage: 1,
+				itemsPerPage: 2
 			});
+
+			expect(args[1].sorting.length).toBe(0);
+			expect(args[1].filters.length).toBe(0);
+		}));
+
+		it('should trow and error without a valid query builder', function() {
+			function invalidQueryBuilder() {
+				instance.findAll(new Date());
+			}
+
+			expect(invalidQueryBuilder).toThrow();
 		});
+	});
 
-		describe('#remove(String id)', function() {
-			it('should remove a single entity found by ID', inject(function($q, $rootScope) {
-				var config = instance.config,
-					dataProvider = config.dataProvider,
-					entityId = 'entity-id';
-
-				spyOn(dataProvider, 'remove').and.returnValue($q.when(true));
-				instance.remove(entityId);
-				$rootScope.$digest();
-
-				expect(dataProvider.remove).toHaveBeenCalledWith(instance.config.name, entityId);
-			}));
-
-			it('should NOT remove if the method is not allowed on dataProvider', function() {
-				var dataProvider = instance.config.dataProvider;
-				spyOn(dataProvider, 'canRemove').and.returnValue(false);
-				spyOn(dataProvider, 'remove');
-
-				instance.remove('id');
-
-				expect(dataProvider.canRemove).toHaveBeenCalled();
-				expect(dataProvider.remove).not.toHaveBeenCalled();
-			});
-		});
-
-		describe('#createQuery()', function() {
-			it('should create a instance of QueryBuilder bound to the repository', inject(function(QueryBuilder) {
-				var query = instance.createQuery();
-				expect(query instanceof QueryBuilder).toBe(true);
-				expect(query.$$repository).toBe(instance.config.name);
-			}));
-		});
-
-		describe('#findAll(QueryBuilder query)', function() {
-			it('should call the dataProvider with the query parameters and return a promise', inject(function($q, QueryBuilder) {
+	describe('#updateContext(RepositoryContext context)', function() {
+		it('should call the dataProvider and update the context data based on context state. ' +
+			'Must be automatically on context changes', inject(function($q, $rootScope) {
+				var context = instance.createContext('test');
 				var dataProvider = instance.dataProvider;
+
+				context.initialize();
+
 				var response = {
 					meta: {
 						count: 100,
@@ -223,94 +253,46 @@ describe('Repository', function() {
 					data: [{}, {}]
 				};
 
-				var qb = QueryBuilder.create()
-					.from(instance.name)
-					.limit(2)
-					.skip(0);
-
-				spyOn(dataProvider, 'findAll').and.returnValue($q.when(response));
-
-				instance.findAll(qb);
-				var args = dataProvider.findAll.calls.argsFor(0);
-
-				expect(args[0]).toBe(qb.$$repository);
-
-				expect(args[1].pagination).toEqual({
-					currentPage: 1,
-					itemsPerPage: 2
-				});
-
-				expect(args[1].sorting.length).toBe(0);
-				expect(args[1].filters.length).toBe(0);
-			}));
-
-			it('should trow and error without a valid query builder', function() {
-				function invalidQueryBuilder() {
-					instance.findAll(new Date());
-				}
-
-				expect(invalidQueryBuilder).toThrow();
-			});
-		});
-
-		describe('#updateContext(RepositoryContext context)', function() {
-			it('should call the dataProvider and update the context data based on context state. ' +
-				'Must be automatically on context changes', inject(function($q, $rootScope) {
-					var context = instance.createContext('test');
-					var dataProvider = instance.dataProvider;
-
-					context.initialize();
-
-					var response = {
-						meta: {
-							count: 100,
-							itemsPerPage: 2,
-							currentPage: 1
-						},
-						data: [{}, {}]
-					};
-
-					spyOn(instance, 'updateContext').and.callThrough();
-					spyOn(dataProvider, 'findAll').and.returnValue($q.when(response));
-
-					context.filters().where('name', 'Bob');
-
-					var contextState = context.toJSON();
-
-					$rootScope.$digest();
-
-					expect(instance.updateContext).toHaveBeenCalledWith(context);
-					expect(dataProvider.findAll).toHaveBeenCalledWith(instance.config.name, contextState);
-					expect(context.data).toBe(response.data);
-
-					var paginationState = context.pagination().toJSON();
-					expect(paginationState.count).toBe(response.meta.count);
-					expect(paginationState.itemsPerPage).toBe(response.meta.itemsPerPage);
-					expect(paginationState.currentPage).toBe(response.meta.currentPage);
-				}));
-
-			it('should call the dataProvider and update the context error if the fetching fails', inject(function($q, $rootScope) {
-				var context = instance.createContext('fail-test');
-				var dataProvider = instance.dataProvider;
-
-				context.initialize();
-
-				var response = {
-					errors: ['some error']
-				};
-
 				spyOn(instance, 'updateContext').and.callThrough();
-				spyOn(dataProvider, 'findAll').and.returnValue($q.reject(response));
+				spyOn(dataProvider, 'findAll').and.returnValue($q.when(response));
 
 				context.filters().where('name', 'Bob');
 
 				var contextState = context.toJSON();
+
 				$rootScope.$digest();
 
 				expect(instance.updateContext).toHaveBeenCalledWith(context);
 				expect(dataProvider.findAll).toHaveBeenCalledWith(instance.config.name, contextState);
-				expect(context.error).toBe(response);
+				expect(context.data).toBe(response.data);
+
+				var paginationState = context.pagination().toJSON();
+				expect(paginationState.count).toBe(response.meta.count);
+				expect(paginationState.itemsPerPage).toBe(response.meta.itemsPerPage);
+				expect(paginationState.currentPage).toBe(response.meta.currentPage);
 			}));
-		});
+
+		it('should call the dataProvider and update the context error if the fetching fails', inject(function($q, $rootScope) {
+			var context = instance.createContext('fail-test');
+			var dataProvider = instance.dataProvider;
+
+			context.initialize();
+
+			var response = {
+				errors: ['some error']
+			};
+
+			spyOn(instance, 'updateContext').and.callThrough();
+			spyOn(dataProvider, 'findAll').and.returnValue($q.reject(response));
+
+			context.filters().where('name', 'Bob');
+
+			var contextState = context.toJSON();
+			$rootScope.$digest();
+
+			expect(instance.updateContext).toHaveBeenCalledWith(context);
+			expect(dataProvider.findAll).toHaveBeenCalledWith(instance.config.name, contextState);
+			expect(context.error).toBe(response);
+		}));
 	});
 });
